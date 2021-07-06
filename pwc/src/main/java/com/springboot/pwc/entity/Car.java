1a49458ea6052643769c346329567f9189d0708a
Neelanjan-Goswami <neelanjan.goswami999@gmail.com>
2021-07-06 22:11:53 +0530

    Update Car.java

--- pwc/src/main/java/com/springboot/pwc/entity/Car.java
+++ pwc/src/main/java/com/springboot/pwc/entity/Car.java
@@ -1,9 +1,19 @@
 package com.springboot.pwc.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,9 +28,19 @@ public class Car {
 	
 	@Id
 	@GeneratedValue
+	@NotNull
+	@Column(name = "car_id")
 	private int car_id;
+	
+	@OneToOne(mappedBy = "car", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private Car_Inventory carinventory;
+	
+	@NotNull
 	private String name;
+	@NotNull
 	private String colour;
+	@NotNull
 	private int price;
 	
 	public int getCar_id() {
